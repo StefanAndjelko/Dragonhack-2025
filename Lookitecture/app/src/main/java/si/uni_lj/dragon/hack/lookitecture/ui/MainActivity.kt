7b66d11b20e1759213a9499a17b1d4902f984776
AngelNau <angelnaumov1@gmail.com>
2025-04-12 17:39:19 +0200

    Re-initialize lookitechture anroid project.

---
+++ Lookitecture/app/src/main/java/si/uni_lj/dragon/hack/lookitecture/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package si.uni_lj.dragon.hack.lookitecture.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import si.uni_lj.dragon.hack.lookitecture.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
