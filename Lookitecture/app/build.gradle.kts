3f00b519efad54a372f660a9273c4883d1085805
AngelNau <angelnaumov1@gmail.com>
2025-04-12 17:58:58 +0200

    Add dependencies for jetpack compose.

--- Lookitecture/app/build.gradle.kts
+++ Lookitecture/app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -41,4 +44,23 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Jetpack Compose dependencies
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    // Material Design 3
+    implementation("androidx.compose.material3:material3")
+    // Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.9.0")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    // Integration with LiveData
+    implementation("androidx.compose.runtime:runtime-livedata")
+    // Integration with RxJava
+    implementation("androidx.compose.runtime:runtime-rxjava2")
 }
