682a0217ecb27fb4606beb4745de044d6fa8b60b
AngelNau <angelnaumov1@gmail.com>
2025-04-12 23:10:05 +0200

    Splash screen finished.

--- Lookitecture/app/src/main/java/si/uni_lj/dragon/hack/lookitecture/ui/MainActivity.kt
+++ Lookitecture/app/src/main/java/si/uni_lj/dragon/hack/lookitecture/ui/MainActivity.kt
@@ -1,14 +1,69 @@
 package si.uni_lj.dragon.hack.lookitecture.ui
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
 import si.uni_lj.dragon.hack.lookitecture.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            // Create your custom animation.
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 500L
+
+            // Call SplashScreenView.remove at the end of your custom animation.
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            // Run your animation.
+            slideUp.start()
+        }
         super.onCreate(savedInstanceState)
+        setContent {
+            Main()
+        }
+    }
+    @Preview
+    @Composable
+    private fun Main() {
+        LaunchedEffect(key1 = true) {
+            delay(2000)
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Color.Yellow
+                ), contentAlignment = Alignment.Center
+        ) {
+            Image(painter = painterResource(id = R.drawable.building), contentDescription = null)
+        }
     }
 }
